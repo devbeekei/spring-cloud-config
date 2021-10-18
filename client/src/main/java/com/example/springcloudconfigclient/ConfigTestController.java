220b225dc9b13102334160233a3f3ea08d950266
devbeekei@wordvice.com <devbeekei@wordvice.com>
2021-10-18 15:04:53 +0900

    Spring Cloud Config ì적용

--- client/src/main/java/com/example/springcloudconfigclient/ConfigTestController.java
+++ client/src/main/java/com/example/springcloudconfigclient/ConfigTestController.java
@@ -1,2 +1,15 @@
-package com.example.springcloudconfigclient;public class ConfigTestController {
+package com.example.springcloudconfigclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigTestController {
+
+    @GetMapping("/config")
+    public String string(@Value("${spring.datasource.url}") String dbUrl) {
+        return dbUrl;
+    }
+
 }
